dadcd25e617a47d289dfea62d59be2b9ef976622
Timo Kaufmann <timokau@zoho.com>
2020-09-30 22:35:20 +0200

    Post review and merge reminders before timeout
    Its easy to miss a notification in the email inbox.

--- marvin/triage.py
+++ marvin/triage.py
@@ -11,12 +11,25 @@ from marvin import gh_util
 from marvin import team
 from marvin import triage_runner
 from marvin.command_router import CommandRouter
+from marvin.gh_util import post_comment
 from marvin.gh_util import set_issue_status
 
 command_router = CommandRouter()
 
+AFTER_WARNING_SECONDS = 60 * 60 * 24 * 1  # one day
 AWAITING_REVIEWER_TIMEOUT_SECONDS = 60 * 60 * 24 * 3  # three days
 AWAITING_MERGER_TIMEOUT_SECONDS = 60 * 60 * 24 * 3  # three days
+
+REVIEW_REMINDER_TEXT = """
+**Reminder: Please review!**
+
+This Pull Request is awaiting review. If you are the assigned reviewer, please have a look. Try to find another reviewer if necessary. If you can't, please say so. If the status is not accurate, please change it. If nothing happens, this PR will be put back in the `needs_reviewer` queue in one day.
+""".strip()
+MERGE_REMINDER_TEXT = """
+**Reminder: Please review!**
+
+Reminder: This Pull Request is **awaiting merger**. If you are the assigned reviewer with commit permission, please have a look. If you can't, please say so. If the status is not accurate, please change it. If nothing happens, this PR will be put back in the `needs_reviewer` queue in one day.
+""".strip()
 
 
 async def timeout_awaiting_reviewer(
@@ -30,6 +43,7 @@ async def timeout_awaiting_reviewer(
             f"repo:{repository_name}",
             "is:open",
             "is:pr",
+            "label:timeout_pending",
             "label:awaiting_reviewer",
             "label:marvin",
             "sort:updated-asc",  # stale first
@@ -38,13 +52,36 @@ async def timeout_awaiting_reviewer(
     async for issue in search_results:
         last_updated = datetime.strptime(issue["updated_at"], "%Y-%m-%dT%H:%M:%S%z")
         age = datetime.now(timezone.utc) - last_updated
-        if age.total_seconds() < AWAITING_REVIEWER_TIMEOUT_SECONDS:
+        if age.total_seconds() < AFTER_WARNING_SECONDS:
             break
 
         print(
             f"awaiting_reviewer -> needs_reviewer: #{issue['number']} ({issue['title']})"
         )
         await set_issue_status(issue, "needs_reviewer", gh, token)
+
+    print("Posting warnings in awaiting_review PRs")
+    search_results = gh_util.search_issues(
+        gh,
+        token,
+        query_parameters=[
+            f"repo:{repository_name}",
+            "is:open",
+            "is:pr",
+            "label:awaiting_reviewer",
+            "-label:timeout_pending",
+            "label:marvin",
+            "sort:updated-asc",
+        ],
+    )
+    async for issue in search_results:
+        last_updated = datetime.strptime(issue["updated_at"], "%Y-%m-%dT%H:%M:%S%z")
+        age = datetime.now(timezone.utc) - last_updated
+        if age.total_seconds() < AWAITING_REVIEWER_TIMEOUT_SECONDS:
+            break
+
+        print(f"awaiting_reviewer reminder: #{issue['number']} ({issue['title']})")
+        await post_comment(gh, token, issue["comments_url"], REVIEW_REMINDER_TEXT)
 
 
 async def timeout_awaiting_merger(
@@ -58,6 +95,7 @@ async def timeout_awaiting_merger(
             f"repo:{repository_name}",
             "is:open",
             "is:pr",
+            "label:timeout_pending",
             "label:awaiting_merger",
             "label:marvin",
             "sort:updated-asc",  # stale first
@@ -66,11 +104,36 @@ async def timeout_awaiting_merger(
     async for issue in search_results:
         last_updated = datetime.strptime(issue["updated_at"], "%Y-%m-%dT%H:%M:%S%z")
         age = datetime.now(timezone.utc) - last_updated
-        if age.total_seconds() < AWAITING_MERGER_TIMEOUT_SECONDS:
+        if age.total_seconds() < AFTER_WARNING_SECONDS:
             break
 
         print(f"awaiting_merger -> needs_merger: #{issue['number']} ({issue['title']})")
         await set_issue_status(issue, "needs_merger", gh, token)
+
+    print("Posting warnings in awaiting_merger PRs")
+    search_results = gh_util.search_issues(
+        gh,
+        token,
+        query_parameters=[
+            f"repo:{repository_name}",
+            "is:open",
+            "is:pr",
+            "label:awaiting_merger",
+            "-label:timeout_pending",
+            "label:marvin",
+            "sort:updated-asc",
+        ],
+    )
+    async for issue in search_results:
+        last_updated = datetime.strptime(issue["updated_at"], "%Y-%m-%dT%H:%M:%S%z")
+        age = datetime.now(timezone.utc) - last_updated
+        if age.total_seconds() < AWAITING_REVIEWER_TIMEOUT_SECONDS:
+            break
+
+        print(f"awaiting_merger reminder: #{issue['number']} ({issue['title']})")
+        await post_comment(
+            gh, token, issue["comments_url"], MERGE_REMINDER_TEXT,
+        )
 
 
 async def assign_mergers(gh: GitHubAPI, token: str, repository_name: str) -> None:
